bd091f08c6ca3af7deb9526864c07eca87c99980
RamTzemah <ram.tzemah@inx.co>
2023-03-26 00:11:09 +0300

    Initial Commit

---
+++ Clima Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima Weather
+//
+//  Created by רם צמח on 26/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
